6cf4fff919a6727996d3a965e633c0b0467f3299
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-28 11:17:27 -0700

    feat: Crear microservicio para eventos

---
+++ backend/plataforma_eventos/microservice_event/src/main/java/com/example/microservice_event/microservice_event/MicroserviceEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_event.microservice_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEventApplication.class, args);
+	}
+
+}
---
+++ backend/plataforma_eventos/microservice_event/src/test/java/com/example/microservice_event/microservice_event/MicroserviceEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_event.microservice_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
