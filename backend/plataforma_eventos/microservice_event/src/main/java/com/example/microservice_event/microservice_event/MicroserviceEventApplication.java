9782bbc0ccd74f93b759ea2a531ce557ce078907
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-29 17:21:04 -0700

    chore: Habilitar el microservicio como cliente de eureka

--- backend/plataforma_eventos/microservice_event/src/main/java/com/example/microservice_event/microservice_event/MicroserviceEventApplication.java
+++ backend/plataforma_eventos/microservice_event/src/main/java/com/example/microservice_event/microservice_event/MicroserviceEventApplication.java
@@ -2,7 +2,9 @@ package com.example.microservice_event.microservice_event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceEventApplication {
 
