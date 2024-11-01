ef525941cd51291c01be2686979fb931a8584de6
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-11-01 14:10:45 -0700

    feat: Crear entidad para User

---
+++ backend/plataforma_eventos/microservice_users/src/main/java/com/example/microservice_users/microservice_users/entities/User.java
@@ -0,0 +1,32 @@
+package com.example.microservice_users.microservice_users.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String telephone;
+    private String password;
+    private String role;
+    private Date createdAt;
+
+}
