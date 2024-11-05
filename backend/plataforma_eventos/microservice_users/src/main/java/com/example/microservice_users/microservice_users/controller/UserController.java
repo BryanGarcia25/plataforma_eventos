5f13e1b93d8624c14ad5d1289bc88f545bbdd989
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-11-04 18:14:24 -0700

    update: Cambiar inyección de la clase UserService por la clase UserServiceImpl

--- backend/plataforma_eventos/microservice_users/src/main/java/com/example/microservice_users/microservice_users/controller/UserController.java
+++ backend/plataforma_eventos/microservice_users/src/main/java/com/example/microservice_users/microservice_users/controller/UserController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservice_users.microservice_users.dto.UserDto;
-import com.example.microservice_users.microservice_users.service.UserService;
+import com.example.microservice_users.microservice_users.service.UserServiceImpl;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,40 +14,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     
-    private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
 
     @PostMapping("/create_user")
     public String createUser(@RequestBody UserDto userDto) {
-        return userService.saveUser(userDto);
+        return userServiceImpl.saveUser(userDto);
     }
 
     @GetMapping("/get_users")
     public List<UserDto> getAllUsers() {
-        return userService.findAllUsers();
+        return userServiceImpl.findAllUsers();
     }
 
     @GetMapping("/get_user/{id}")
     public UserDto getUserById(@PathVariable int id) {
-        return userService.findUserById(id);
+        return userServiceImpl.findUserById(id);
     }
 
     @PutMapping("/update_user/{id}")
     public String updateUserById(@PathVariable int id, @RequestBody UserDto userDto) {
-        return userService.updateUser(id, userDto);
+        return userServiceImpl.updateUser(id, userDto);
     }
 
     @DeleteMapping("/delete_user/{id}")
     public String deleteUserById(@PathVariable int id) {
-        return userService.deleteUser(id);
+        return userServiceImpl.deleteUser(id);
     }
 
 }
