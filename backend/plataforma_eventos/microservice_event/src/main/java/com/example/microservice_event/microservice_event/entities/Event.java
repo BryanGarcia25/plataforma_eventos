d485ba010de8d1c12de4a785762cead098e7d262
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-11-04 17:27:58 -0700

    feat: Crear entidad para Event

---
+++ backend/plataforma_eventos/microservice_event/src/main/java/com/example/microservice_event/microservice_event/entities/Event.java
@@ -0,0 +1,31 @@
+package com.example.microservice_event.microservice_event.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private String address;
+    private Date dateEvent;
+    private int numberSeats;
+    private double price;
+    private Date createdAt;
+}
